a2613cda2e6bb3066c9aa9378f7fb2d370bdd51c
Mattia <mattia.tummolilllo@gmail.com>
2020-10-28 23:07:43 +0100

    Esercizio completato (console.log)

--- js/script.js
+++ js/script.js
@@ -1,41 +1,59 @@
-// GENERAZIONE LISTA 16 (BOMBE)
-var bombsNum = [];
+// creato solo per fixare i mille bug del prompt riguardo la console, dando il tempo di aprirla
+var gioca = document.getElementById("gioca");
+gioca.addEventListener("click",
+	function() {
+      // GENERAZIONE LISTA 16 (BOMBE)
+      var bombsList = ["Numeri bomba -> "];
 
-while (bombsNum.length < 16) {
-   var bombsElement = randomNum(100);
+      while (bombsList.length < 16) {
+         var bombsElement = randomNum(100);
+         if (bombsList.includes(bombsElement) == false) {
+            bombsList.push(bombsElement);
+         }
+      }
+      // discommentare questo console.log per vedere in quali numeri si trovano le bombe
+      // console.log(bombsList);
 
-   if (bombsNum.includes(bombsElement) == false) {
-      bombsNum.push(bombsElement);
-   }
-}
-console.log(bombsNum);
+
+      // GENERAZIONE RICHIESTE NUMERO UTENTE/ BOMBE / CASO DELL'INSERIMENTO NUMERI UGUALI
+      // caso mina non trovata
+      var minaTrovata = false;
+      //salvataggio valori inseriti dall'utente
+      var userNumList = [];
+      var i = 0;
+      while (i < 84 && minaTrovata == false) {
+         // chiedo all'utente di inserire un numero
+         var userNum = parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
+         // controllo che il numero inserito si trovi nel range indicato
+         if (userNum > 100 || userNum < 1) {
+            alert("Questo numero non è compreso tra 1 e 100 !")
+         }
+
+         // controllo mina trovata
+         if (bombsList.includes(userNum) == true) {
+            minaTrovata = true;
+            alert("BUM, hai perso !");
+         }
+
+         // controllo doppione numero inserito
+         if (userNumList.includes(userNum) == true) {
+            alert("Hai già usato questo numero, riprova !");
+         } else {
+            // push valori di input nell'array salvataggio
+            userNumList.push(userNum);
+            console.log("Numeri che hai già utilizzato: " + userNumList);
+         }
 
 
-// richiesta x86
-// <84  mina trovata stesso numero
+         i++;
+      }
 
-var minaTrovata = false;
-var i = 0;
-while (i < 84 && minaTrovata == false && theSameNum == false) {
-   // caso mina trovata
-   var userNum = parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
-   if (bombsNum.includes(userNum) == true) {
-      minaTrovata = true;
-      alert("BUM, hai perso !");
-   }
+      // score
+      alert("Hai fatto : " + (--i) + " punti");
+      location.reload();
 
-   // caso doppione numero inserito
-   var theSameNum = false;
-   var userNumList = [];
-   userNumList.push(userNum);
-   userNumList(5).fill(userNum);
-   if ((userNumList.includes(userNum) == userNum) {
-      console.log("uguale");
-   }
-   console.log(userNumList);
-   i++;
-}
-alert("Hai fatto : " + (i-1) + " punti");
+	}
+);
 
 
 
